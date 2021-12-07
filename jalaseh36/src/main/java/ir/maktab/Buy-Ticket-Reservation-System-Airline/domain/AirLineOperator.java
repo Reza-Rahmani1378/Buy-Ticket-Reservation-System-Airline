66c1136708571e903402b6cdbeed92513ef4be3a
Reza-Rahmani1378 <reza6273757@gmail.com>
2021-12-07 14:00:39 +0330

    add AirlineOperator Domain

---
+++ jalaseh36/src/main/java/ir/maktab/Buy-Ticket-Reservation-System-Airline/domain/AirLineOperator.java
@@ -0,0 +1,44 @@
+package ir.maktab.home_work17.domain;
+
+import ir.maktab.home_work17.domain.enumeration.UserType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import java.sql.Date;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class AirLineOperator extends User {
+
+    @OneToOne
+    private AirLine airLine;
+
+    public AirLineOperator(String firstName,
+                           String lastName,
+                           String username,
+                           String national_code,
+                           String password,
+                           Date birth_day,
+                           UserType userType,
+                           boolean isAirLineOperator) {
+        super(firstName, lastName, username, national_code, password, birth_day, userType, isAirLineOperator);
+
+    }
+
+    public AirLineOperator(
+            String firstName,
+            String lastName,
+            String national_code,
+            String username,
+            String password,
+            UserType userType,
+            boolean isAirLineOperator) {
+        super(firstName, lastName, national_code, username, password, userType, isAirLineOperator);
+    }
+
+}
