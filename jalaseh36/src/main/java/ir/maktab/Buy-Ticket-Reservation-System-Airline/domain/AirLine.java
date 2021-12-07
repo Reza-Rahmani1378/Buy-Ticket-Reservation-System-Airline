caa571ea43b222bd071d7e5ee744996ee39c19ad
Reza-Rahmani1378 <reza6273757@gmail.com>
2021-12-07 14:03:44 +0330

    add Airline Domain

---
+++ jalaseh36/src/main/java/ir/maktab/Buy-Ticket-Reservation-System-Airline/domain/AirLine.java
@@ -0,0 +1,40 @@
+package ir.maktab.home_work17.domain;
+
+import ir.maktab.home_work17.base.domain.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = AirLine.TABLE_NAME)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AirLine extends BaseEntity<Long> {
+    public static final String TABLE_NAME = "airline_table";
+
+    @Column
+    private String name;
+
+    @Column
+    private String concat_us;
+    @OneToMany(mappedBy = "air_line", cascade = CascadeType.ALL)
+    private List<Ticket> tickets;
+    @OneToOne(mappedBy = "airLine", cascade = CascadeType.ALL)
+    private AirLineOperator airLineOperator;
+
+//    @OneToOne(cascade = CascadeType.ALL)
+//    private Customer customer;
+
+    public AirLine(String name, String concat_us, AirLineOperator airLineOperator) {
+        this.name = name;
+        this.concat_us = concat_us;
+        this.airLineOperator = airLineOperator;
+    }
+
+}
