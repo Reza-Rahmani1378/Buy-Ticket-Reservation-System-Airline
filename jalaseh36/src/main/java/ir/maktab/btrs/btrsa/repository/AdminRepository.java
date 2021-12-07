00e1ee789d23ff561b8a6a8dbb5d9363be754486
Reza-Rahmani1378 <reza6273757@gmail.com>
2021-12-07 14:43:02 +0330

    add repository and impl for Admin

---
+++ jalaseh36/src/main/java/ir/maktab/btrs/btrsa/repository/AdminRepository.java
@@ -0,0 +1,7 @@
+package ir.maktab.btrs.btrsa.repository;
+
+import ir.maktab.btrs.btrsa.base.repository.BaseRepository;
+import ir.maktab.btrs.btrsa.domain.Admin;
+
+public interface AdminRepository extends BaseRepository<Admin, Long> {
+}
---
+++ jalaseh36/src/main/java/ir/maktab/btrs/btrsa/repository/impl/AdminRepositoryImpl.java
@@ -0,0 +1,20 @@
+package ir.maktab.btrs.btrsa.repository.impl;
+
+import ir.maktab.btrs.btrsa.base.repository.impl.BaseRepositoryImpl;
+import ir.maktab.btrs.btrsa.domain.Admin;
+import ir.maktab.btrs.btrsa.repository.AdminRepository;
+
+import javax.persistence.EntityManager;
+
+public class AdminRepositoryImpl extends BaseRepositoryImpl<Admin, Long> implements AdminRepository {
+    public AdminRepositoryImpl(EntityManager entityManager) {
+        super(entityManager);
+    }
+
+    @Override
+    public Class<Admin> getEntityClass() {
+        return Admin.class;
+    }
+
+
+}
